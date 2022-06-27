5e647b60d7785c767b134d144a163d48296da69e
tasyacitta <rrlydiadevina@gmail.com>
2022-06-27 19:26:28 +0700

    handle cors

--- src/main/java/com/example/demo/config/CorsConfig.java
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -8,17 +8,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig{
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
+	// @Bean
+	// public WebMvcConfigurer corsConfigurer() {
+	// 	return new WebMvcConfigurer() {
 			
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-					.allowedMethods("GET", "POST", "PUT", "DELETE")
-					.allowedHeaders("*")
-					.allowedOrigins("http://localhost:3000");
-			}
-		};
-	}
+	// 		@Override
+	// 		public void addCorsMappings(CorsRegistry registry) {
+	// 			registry.addMapping("/**")
+	// 				.allowedMethods("GET", "POST", "PUT", "DELETE")
+	// 				.allowedHeaders("*")
+	// 				.allowedOrigins("http://localhost:3000");
+	// 		}
+	// 	};
+	// }
 }
