b9368320f7ee4096902aac5091054c6620d3e395
tasyacitta <rrlydiadevina@gmail.com>
2022-06-27 17:45:25 +0700

    handle cors

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -13,14 +13,14 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-	@Bean
-   public WebMvcConfigurer corsConfigurer() {
-      return new WebMvcConfigurerAdapter() {
-         @Override
-         public void addCorsMappings(CorsRegistry registry) {
-            registry.addMapping("/api/**").allowedOrigins("https://booking-vaksin-alta.herokuapp.com");
-         }
-      };
-   }
+	// @Bean
+   // public WebMvcConfigurer corsConfigurer() {
+   //    return new WebMvcConfigurerAdapter() {
+   //       @Override
+   //       public void addCorsMappings(CorsRegistry registry) {
+   //          registry.addMapping("/api/**").allowedOrigins("https://booking-vaksin-alta.herokuapp.com");
+   //       }
+   //    };
+   // }
 
 }
