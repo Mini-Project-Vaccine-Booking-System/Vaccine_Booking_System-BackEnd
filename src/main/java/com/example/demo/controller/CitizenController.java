a0b4332b44c6a67daff6617a5874786a6b103b42
tasyacitta <rrlydiadevina@gmail.com>
2022-07-02 22:50:40 +0700

    user handling 415

--- src/main/java/com/example/demo/controller/CitizenController.java
+++ src/main/java/com/example/demo/controller/CitizenController.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value= "/user", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(value= "/user")
 public class CitizenController {
     @Autowired
     private UserService userService;
